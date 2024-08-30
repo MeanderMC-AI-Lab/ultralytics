5c669613481ed530edad7720175f3b9619d6d9a2
vribbens <vincentribbens@hotmail.com>
2024-08-30 07:45:11 +0000

    set classweights and incorporate mAP90

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -163,7 +163,7 @@ class v8DetectionLoss:
         h = model.args  # hyperparameters
 
         m = model.model[-1]  # Detect() module
-        self.bce = nn.BCEWithLogitsLoss(reduction="none")
+        self.bce = nn.BCEWithLogitsLoss(weight= torch.tensor([0,0,1,1,0,1,1]).to(device), reduction="none")
         self.hyp = h
         self.stride = m.stride  # model strides
         self.nc = m.nc  # number of classes
